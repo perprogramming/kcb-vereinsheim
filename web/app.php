9acc33a20ec8bc54e8196bd261bb96d2d1ce6936
SSH User <ssh-49905-plb@chensit.ispgateway.de>
2012-10-06 16:26:28 +0200

    bugfixes

--- web/app.php
+++ web/app.php
@@ -1,12 +1,10 @@
+
 <?php
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
